4e0e48bf73b469be86918fe9cdcc7a89376ee461
Sashimye <207444724+Sashimye@users.noreply.github.com>
2026-03-19 11:06:12 +0100

    6 exos done

--- src/5-arrayToFrench.js
+++ src/5-arrayToFrench.js
@@ -9,4 +9,7 @@
  *      arrayToFrench(['un', 'deux', 'trois', 'soleil']); // returns "un, deux, trois et soleil"
  *      arrayToFrench(['noir', 'blanc']); // returns "noir et blanc"
  */
-export const arrayToFrench = (array) => {};
+export const arrayToFrench = (array) => {
+    if (array.length <= 2) return array.join(" et ");
+    return array.slice(0, -2).join(", ") + ", " + array.slice(-2).join(" et ");
+};
